19bf14a33eb7abd4bd4ca3635238610a2da94be7
greg pereira <grpereir@redhat.com>
2023-11-29 14:05:26 -0800

    openshift enablement

--- database-deployer-openshift.sh
+++ database-deployer-openshift.sh
@@ -1,5 +1,11 @@
-
-yq '.spec.template.spec.containers.0 += {"securityContext": {"allowPrivilegeEscalation": false, "capabilities": {"drop": ["ALL"]}}}' 02-deployment.yaml > 02-deployment.yaml
-oc apply -f --kustomize ./
+oc apply -f 00-namespace.yaml
+oc project mysql
+oc apply -f 01-pvc.yaml
+yq 'del(.imagePullSecrets)' 01-sa.yaml > 01-openshift-sa.yaml 
+oc apply -f 01-openshift-sa.yaml
+oc apply -f 01-secret.yaml
+oc apply -f 02-service.yaml
+yq '.spec.template.spec.containers.0 += {"securityContext": {"allowPrivilegeEscalation": false, "seccompProfile": {"type": "RuntimeDefault"}, "capabilities": {"drop": ["ALL"]}}}' 02-deployment.yaml > 02-openshift-deployment.yaml
+oc apply -f 02-openshift-deployment.yaml
 oc create secret generic pull-sec -n mysql --from-file=.dockerconfigjson=/home/`whoami`/.docker/config.json --type=kubernetes.io/dockerconfigjson
-oc expose svc trillian-mysql --port=3306 --name trillian-mysql-route
+oc expose svc trillian-mysql -n mysql --port=3306 --name trillian-mysql-route
